df50df48d0ab4c5e52689df4d54cba23ad8142bc
Pav <pav0107@gmail.com>
2020-01-05 13:24:21 +0000

    customising robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,7 +36,7 @@ class App extends Component {
         } else {
             return (
                 <div className='tc'>
-                    <h1 className='f2'>RoboFriends</h1>
+                    <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
