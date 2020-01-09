16427b4c30bef4eede205d6b203ddfb7e8def6b9
Pav <pav0107@gmail.com>
2020-01-09 17:27:40 +0000

    change in height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', height: '800px'}}>
+        <div style={{overflowY: 'scroll', height: '1500px'}}>
             {props.children}
         </div>
     );
